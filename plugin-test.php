cadbf74b49fc9260898194e16ae33e8a2d60d841
Florian VALOIS <contact@florian-valois.com>
2019-12-24 11:37:42 +0100

    v1.0.3

--- plugin-test.php
+++ plugin-test.php
@@ -3,7 +3,7 @@
 Plugin Name: Plugin Test
 Plugin URI: 
 Description:  A boilerplate plugin for WordPress with many options 
-Version: 1.0.2
+Version: 1.0.3
 Author: Florian Valois
 Author URI: https://florian-valois.com
 Text Domain: plugin-test
